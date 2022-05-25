b54d24934bade9a9cab6841db2cf123a9fbba2e4
vitalekp <vitalekpushkarov@gmail.com>
2022-05-25 13:58:53 +0300

    render todo title

---
+++ lesson1-intro/todo-list/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const rootElem = document.querySelector('#root');
+
+const listTitle = <h1 class="title">Todo List</h1>;
+
+ReactDOM.render(listTitle, rootElem);
