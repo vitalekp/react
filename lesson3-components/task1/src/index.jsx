69d479755da695b7c63eb2b60cb3b999919c8c59
vitalekp <vitalekpushkarov@gmail.com>
2022-05-30 13:39:51 +0300

    component todolist

--- lesson3-components/task1/src/index.jsx
+++ lesson3-components/task1/src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { SearchField } from './Search.jsx';
+import { Search } from './Search.jsx';
 import './search.scss';
 import './index.scss';
 
 const rootElem = document.querySelector('#root');
 
-ReactDOM.render(<SearchField name="Bob" />, rootElem);
+ReactDOM.render(<Search name="Bob" />, rootElem);
