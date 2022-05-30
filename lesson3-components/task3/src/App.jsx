fdb12866e98435e2fee3f4d31e6082337910bbe6
vitalekp <vitalekpushkarov@gmail.com>
2022-05-30 17:21:33 +0300

    fix

--- lesson3-components/task3/src/App.jsx
+++ lesson3-components/task3/src/App.jsx
@@ -7,7 +7,7 @@ const userInfo = {
 };
 
 const App = () => {
-  return <Comment user={userInfo} text="Good job!" date={new Date()} />;
+  return <Comment author={userInfo} text="Good job!" date={new Date()} />;
 };
 
 export default App;
