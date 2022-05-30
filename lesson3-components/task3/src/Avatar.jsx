8b1844aaa5775313a7de5da3ee5d615836e550da
vitalekp <vitalekpushkarov@gmail.com>
2022-05-30 17:10:38 +0300

    fix

--- lesson3-components/task3/src/Avatar.jsx
+++ lesson3-components/task3/src/Avatar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 const Avatar = props => {
-  return <img className="avatar" src={props.avatarUrl} alt={props.name} />;
+  return <img className="avatar" src={props.avatarUrl} alt={props.author} />;
 };
 
 export default Avatar;
