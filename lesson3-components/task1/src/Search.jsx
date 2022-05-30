4c8138f2021acd2d908a3850f952c4cc3be8bddd
vitalekp <vitalekpushkarov@gmail.com>
2022-05-30 13:38:49 +0300

    fix

--- lesson3-components/task1/src/Search.jsx
+++ lesson3-components/task1/src/Search.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 // algo
 // 1 створити компоненту SearchField (props), відмалювати в #root
 
-export const SearchField = props => {
+export const SearchField = name => {
   return (
     <div className="search">
-      <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+      <h1 className="search__title">{`Hello, ${name.name}. What to search for you?`}</h1>
       <div className="search__field">
         <input type="text" class="search__input" />
         <button className="search__button">Search</button>
