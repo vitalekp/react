f94e24214f98755cddab2a7a5154e372588296e3
vitalekp <vitalekpushkarov@gmail.com>
2022-06-01 17:03:17 +0300

    Conditional rendering

---
+++ lesson6-cond-rendering/task1/src/Auth.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Greeting from './Greeting';
+import Login from './Login';
+import Logout from './Logout';
+
+class Auth extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+  handleLogin = () => {
+    this.setState({
+      isLoggedIn: true,
+    });
+  };
+
+  handleLogout = () => {
+    this.setState({
+      isLoggedIn: false,
+    });
+  };
+
+  render() {
+    const btn = this.state.isLoggedIn ? (
+      <Logout onLogout={this.handleLogout} />
+    ) : (
+      <Login onLogin={this.handleLogin} />
+    );
+
+    return (
+      <div className="panel">
+        <Greeting isLoggedIn={this.state.isLoggedIn} />
+        {btn}
+      </div>
+    );
+  }
+}
+
+export default Auth;
---
+++ lesson6-cond-rendering/task1/src/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserGreeting from './userGreeting';
+import GuestGreeting from './GuestGreeting';
+
+const Greeting = ({ isLoggedIn }) => {
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+};
+
+export default Greeting;
