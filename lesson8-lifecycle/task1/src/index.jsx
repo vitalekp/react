cd2c0c752de8b1bd1e4b1e8e7f1cd99c8e9b43e0
vitalekp <vitalekpushkarov@gmail.com>
2022-06-06 09:36:50 +0300

    lifecycle methods

---
+++ lesson8-lifecycle/task1/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Life from './Life';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Life />, rootElement);
