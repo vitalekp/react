e680e788dc5bf29c5ae2b7fc06a96595a2a11c30
vitalekp <vitalekpushkarov@gmail.com>
2022-06-06 12:53:52 +0300

    event subscription

---
+++ lesson8-lifecycle/task3/src/Dimensions.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+// algo Dimensions
+// 1 add innerWidth & innerHeigth to state +++
+// 2 ініціалізувати в DidMount() дані розміру сторінки +++
+// 3  створити метод onResize, який буде оновлювати розмір сторінки +++
+// 4 створити метод setDimensions, який дізнається розмір сторінки +++
+// 5 відписатий від подій WillUnmount() +++
+
+class Dimensions extends Component {
+  state = {
+    innerWidth: null,
+    innerHeight: null,
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+    const { innerWidth, innerHeight } = window;
+
+    this.setDimensions(innerWidth, innerHeight);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize = e => {
+    const { innerWidth, innerHeight } = e.target;
+    this.setDimensions(innerWidth, innerHeight);
+  };
+
+  setDimensions = (width, heigth) => {
+    this.setState({
+      innerWidth: width,
+      innerHeight: heigth,
+    });
+    document.title = `${innerHeight} - ${innerWidth}`;
+  };
+
+  render() {
+    return (
+      <div className="dimensions">{`${this.state.innerHeight}px - ${this.state.innerWidth}px`}</div>
+    );
+  }
+}
+
+export default Dimensions;
