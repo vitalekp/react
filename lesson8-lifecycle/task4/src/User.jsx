a009b634273514b98b04c4c00f345a0a76ab43a1
vitalekp <vitalekpushkarov@gmail.com>
2022-06-07 06:29:31 +0300

    fetch DidMount

--- lesson8-lifecycle/task4/src/User.jsx
+++ lesson8-lifecycle/task4/src/User.jsx
@@ -34,7 +34,7 @@ class User extends Component {
       <div className="user">
         <img alt="User Avatar" src={avatar_url} className="user__avatar"></img>
         <div className="user__info">
-          <span className="user__name">{name} </span>
+          <span className="user__name">{name}</span>
           <span className="user__location">{location}</span>
         </div>
       </div>
