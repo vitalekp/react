a05e9016dd2fef9cf9b92c2ffbb6dd7dd5f4ef7b
vitalekp <vitalekpushkarov@gmail.com>
2022-06-09 18:06:09 +0300

    fix

--- lesson7-lists/task4/src/Pagination.jsx
+++ lesson7-lists/task4/src/Pagination.jsx
@@ -3,15 +3,13 @@ import React from 'react';
 const Pagination = ({ curPage, goNext, goPrev, itemPerPage, totalItems }) => {
   const isPrevPageAvailable = curPage === 1;
   const isNextPageAvailable = curPage === Math.ceil(totalItems / itemPerPage);
-  // const isNextPageAvailable = curPage * itemPerPage >= totalItems;
-
   return (
     <div className="pagination">
-      <button className="btn" onClick={goPrev} disabled={isPrevPageAvailable}>
+      <button disabled={isPrevPageAvailable} onClick={goPrev} className="btn">
         {isPrevPageAvailable ? '' : '←'}
       </button>
       <span className="pagination__page">{curPage}</span>
-      <button className="btn" onClick={goNext} disabled={isNextPageAvailable}>
+      <button disabled={isNextPageAvailable} onClick={goNext} className="btn">
         {isNextPageAvailable ? '' : '→'}
       </button>
     </div>
@@ -19,3 +17,24 @@ const Pagination = ({ curPage, goNext, goPrev, itemPerPage, totalItems }) => {
 };
 
 export default Pagination;
+
+// import React from 'react';
+
+// const Pagination = ({ curPage, goNext, goPrev, itemPerPage, totalItems }) => {
+//   const isPrevPageAvailable = curPage === 1;
+//   const isNextPageAvailable = curPage * itemPerPage >= totalItems;
+
+//   return (
+//     <div className="pagination">
+//       <button className="btn" onClick={goPrev} disabled={isPrevPageAvailable}>
+//         {isPrevPageAvailable ? '' : '←'}
+//       </button>
+//       <span className="pagination__page">{curPage}</span>
+//       <button className="btn" onClick={goNext} disabled={isNextPageAvailable}>
+//         {isNextPageAvailable ? '' : '→'}
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Pagination;
--- lesson7-lists/task4/src/index.jsx
+++ lesson7-lists/task4/src/index.jsx
@@ -1,38 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import UsersList from './UsersList';
+import App from './App';
 
 const rootElement = document.querySelector('#root');
 
-const users = [
-  { id: 1, name: 'Tom', age: 20 },
-  { id: 12, name: 'Bob', age: 100 },
-  { id: 123, name: 'Rob', age: 32 },
-  { id: 1234, name: 'Rim', age: 45 },
-  { id: 12345, name: 'Skin', age: 27 },
-  { id: 123456, name: 'Bobby', age: 26 },
-  { id: 1234567, name: 'Jack', age: 19 },
-  { id: 12345678, name: 'Bom', age: 21 },
-  { id: 123456789, name: 'DOM', age: 22 },
-  { id: 123456789, name: 'DOM', age: 22 },
-];
-
-ReactDOM.render(<UsersList users={users} />, rootElement);
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.scss';
-// import App from './App';
-
-// const rootElement = document.querySelector('#root');
-
-// ReactDOM.render(<App />, rootElement);
-
-// import React from 'react';
-
-// const App = () => {
-//   return ;
-// };
-
-// export default App;
+ReactDOM.render(<App />, rootElement);
