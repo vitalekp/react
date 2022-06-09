639262d647e2c0b1c43db30bcd784ab34d5ec17b
vitalekp <vitalekpushkarov@gmail.com>
2022-06-09 18:52:19 +0300

    fix

--- lesson7-lists/task4/src/Pagination.jsx
+++ lesson7-lists/task4/src/Pagination.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const Pagination = ({ currentPage, goNext, goPrev, itemsPerPage, totalItems }) => {
-  const isPrevPageAvailable = currentPage === 1;
-  const isNextPageAvailable = currentPage * itemsPerPage >= totalItems;
+  const isPrevPageUnavailable = currentPage === 1;
+  const isNextPageUnavailable = currentPage * itemsPerPage >= totalItems;
   return (
     <div className="pagination">
-      <button className="btn" onClick={goPrev} disabled={isPrevPageAvailable}>
-        {isPrevPageAvailable ? '' : '←'}
+      <button className="btn" onClick={goPrev} disabled={isPrevPageUnavailable}>
+        {isPrevPageUnavailable ? '' : '←'}
       </button>
       <span className="pagination__page">{currentPage}</span>
-      <button className="btn" onClick={goNext} disabled={isNextPageAvailable}>
-        {isNextPageAvailable ? '' : '→'}
+      <button className="btn" onClick={goNext} disabled={isNextPageUnavailable}>
+        {isNextPageUnavailable ? '' : '→'}
       </button>
     </div>
   );
