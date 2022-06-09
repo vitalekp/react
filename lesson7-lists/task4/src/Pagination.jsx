4437ec1d5a939a3c6831434139d6f87b8de00e43
vitalekp <vitalekpushkarov@gmail.com>
2022-06-09 18:50:39 +0300

    fix

--- lesson7-lists/task4/src/Pagination.jsx
+++ lesson7-lists/task4/src/Pagination.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 
 const Pagination = ({ currentPage, goNext, goPrev, itemsPerPage, totalItems }) => {
-  const isPrevPageAvailable = currentPage > 1;
-  const isNextPageAvailable = totalItems - itemsPerPage * currentPage > 0;
-
+  const isPrevPageAvailable = currentPage === 1;
+  const isNextPageAvailable = currentPage * itemsPerPage >= totalItems;
   return (
     <div className="pagination">
-      <button className="btn" onClick={goPrev} disabled={!isPrevPageAvailable}>
-        {isPrevPageAvailable ? '←' : ''}
+      <button className="btn" onClick={goPrev} disabled={isPrevPageAvailable}>
+        {isPrevPageAvailable ? '' : '←'}
       </button>
       <span className="pagination__page">{currentPage}</span>
-      <button className="btn" onClick={goNext} disabled={!isNextPageAvailable}>
-        {isNextPageAvailable ? '→' : ''}
+      <button className="btn" onClick={goNext} disabled={isNextPageAvailable}>
+        {isNextPageAvailable ? '' : '→'}
       </button>
     </div>
   );
@@ -19,5 +18,23 @@ const Pagination = ({ currentPage, goNext, goPrev, itemsPerPage, totalItems }) =
 
 export default Pagination;
 
-//   const isPrevPageAvailable = curPage === Math.ceil(totalItems / itemPerPage);
-//   const isNextPageAvailable = curPage === 1;
+// import React from 'react';
+
+// const Pagination = ({ currentPage, goNext, goPrev, itemsPerPage, totalItems }) => {
+//   const isPrevPageAvailable = currentPage > 1;
+//   const isNextPageAvailable = totalItems - itemsPerPage * currentPage > 0;
+
+//   return (
+//     <div className="pagination">
+//       <button className="btn" onClick={goPrev} disabled={!isPrevPageAvailable}>
+//         {isPrevPageAvailable ? '←' : ''}
+//       </button>
+//       <span className="pagination__page">{currentPage}</span>
+//       <button className="btn" onClick={goNext} disabled={!isNextPageAvailable}>
+//         {isNextPageAvailable ? '→' : ''}
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Pagination;
