1d0af86e139c5b7e9f15e4b4ba1747c5f80c64e1
vitalekp <vitalekpushkarov@gmail.com>
2022-06-09 18:30:51 +0300

    fix

--- lesson7-lists/task4/src/Pagination.jsx
+++ lesson7-lists/task4/src/Pagination.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const Pagination = ({ curPage, goNext, goPrev, itemPerPage, totalItems }) => {
-  const isPrevPageInaccessible = curPage === 1;
-  const isNextPageInaccessible = curPage * itemPerPage >= totalItems;
+  const prevPageAvailable = curPage > 1;
+  const nextPageAvailable = totalItems - itemPerPage * curPage > 0;
 
   return (
     <div className="pagination">
-      <button className="btn" onClick={goPrev} disabled={isPrevPageInaccessible}>
-        {isPrevPageInaccessible ? '' : '←'}
+      <button className="btn" onClick={goPrev} disabled={!prevPageAvailable}>
+        {prevPageAvailable ? '←' : ''}
       </button>
       <span className="pagination__page">{curPage}</span>
-      <button className="btn" onClick={goNext} disabled={isNextPageInaccessible}>
-        {isNextPageInaccessible ? '' : '→'}
+      <button className="btn" onClick={goNext} disabled={!nextPageAvailable}>
+        {nextPageAvailable ? '→' : ''}
       </button>
     </div>
   );
@@ -19,20 +19,5 @@ const Pagination = ({ curPage, goNext, goPrev, itemPerPage, totalItems }) => {
 
 export default Pagination;
 
-// const Pagination = ({ curPage, goNext, goPrev, itemPerPage, totalItems }) => {
 //   const isPrevPageAvailable = curPage === Math.ceil(totalItems / itemPerPage);
 //   const isNextPageAvailable = curPage === 1;
-//   return (
-//     <div className="pagination">
-//       <button disabled={isNextPageAvailable} onClick={goPrev} className="btn">
-//         {isNextPageAvailable ? '' : '←'}
-//       </button>
-//       <span className="pagination__page">{curPage}</span>
-//       <button disabled={isPrevPageAvailable} onClick={goNext} className="btn">
-//         {isPrevPageAvailable ? '' : '→'}
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Pagination;
