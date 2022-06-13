c84ccba01b0a54f470c599b653b61fd2fb321a3c
vitalekp <vitalekpushkarov@gmail.com>
2022-06-13 17:15:53 +0300

    tic-tac-toe

--- lesson9-forms/task5/src/Game.jsx
+++ lesson9-forms/task5/src/Game.jsx
@@ -56,7 +56,7 @@ class Game extends Component {
 
     let status;
     if (winner) {
-      status = 'Winner ' + winner;
+      status = 'Winner: ' + winner;
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
