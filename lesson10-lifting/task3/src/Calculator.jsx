096c0ab34d1928de2c2ac10e17e919bf62d755c7
vitalekp <vitalekpushkarov@gmail.com>
2022-06-15 13:45:29 +0300

    temp calc

--- lesson10-lifting/task3/src/Calculator.jsx
+++ lesson10-lifting/task3/src/Calculator.jsx
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import TemperatureInput from './TemperatureInput';
 
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
 class Calculator extends Component {
   state = {
     temperature: '',
@@ -39,21 +57,3 @@ class Calculator extends Component {
 }
 
 export default Calculator;
-
-function toCelsius(fahrenheit) {
-  return ((fahrenheit - 32) * 5) / 9;
-}
-
-function toFahrenheit(celsius) {
-  return (celsius * 9) / 5 + 32;
-}
-
-function tryConvert(temperature, convert) {
-  const input = parseFloat(temperature);
-  if (Number.isNaN(input)) {
-    return '';
-  }
-  const output = convert(input);
-  const rounded = Math.round(output * 1000) / 1000;
-  return rounded.toString();
-}
