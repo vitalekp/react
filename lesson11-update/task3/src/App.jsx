731a414271cf1e108307eac47edeca250662b0b0
vitalekp <vitalekpushkarov@gmail.com>
2022-06-16 12:45:57 +0300

    expand content

--- lesson11-update/task3/src/App.jsx
+++ lesson11-update/task3/src/App.jsx
@@ -8,29 +8,11 @@ import Expand from './Expand';
 // 4. declare state (what, where) and props
 // 5. write logic
 
-// state
-// isOpen
-
 class App extends Component {
-  state = {
-    isOpen: false,
-    iconStyle: 'fa-solid fa-angle-down',
-  };
-
-  hideBlock = () => {
-    this.setState({
-      isOpen: !this.state.isOpen,
-      iconStyle: this.state.isOpen ? 'fa-solid fa-angle-down' : 'fas fa-chevron-up',
-    });
-  };
-
   render() {
-    const { hideBlock, showBlock } = this;
-    const { isOpen, iconStyle } = this.state;
-
     return (
       <div className="app">
-        <Expand title="Some title" isOpen={isOpen} iconStyle={iconStyle} onClose={hideBlock}>
+        <Expand title="Some title">
           <p>
             Hooks are a new addition in React 16.8. They let you use state and other React features
             without writing a className.
--- lesson11-update/task3/src/Expand.jsx
+++ lesson11-update/task3/src/Expand.jsx
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Expand = ({ children, title, iconStyle, isOpen, onClose }) => {
-  const Open = isOpen ? <div className="expand__content">{children}</div> : null;
+// state
+// isOpen
 
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn" onClick={onClose}>
-          <i className={iconStyle}></i>
-        </button>
+class Expand extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  toogleBlock = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
+  render() {
+    const { toogleBlock } = this;
+    const { isOpen } = this.state;
+    const expandContent = isOpen ? (
+      <div className="expand__content">{this.props.children}</div>
+    ) : null;
+
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{this.props.title}</span>
+          <button className="expand__toggle-btn" onClick={toogleBlock}>
+            {isOpen ? (
+              <i className="fa-solid fa-angle-up"></i>
+            ) : (
+              <i className="fa-solid fa-angle-down"></i>
+            )}
+          </button>
+        </div>
+        {expandContent}
       </div>
-      {Open}
-      {/* <div className="expand__content">{children}</div> */}
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Expand;
 
