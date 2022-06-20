ff0d965b56e6d61497e2b49f842192bde1ab40d6
vitalekp <vitalekpushkarov@gmail.com>
2022-06-20 12:37:22 +0300

    useEffect fetch

---
+++ lesson14-hooks/task2/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import User from './User';
+
+const App = () => {
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <div className="page__content">
+          <h1>Users</h1>
+          <ul className="navigation">
+            <li className="navigation__item">
+              <Link to="/users/github">Github</Link>
+            </li>
+            <li className="navigation__item">
+              <Link to="/users/facebook">Facebook</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/users/:usersId" component={User} />
+            <Route path="/">
+              <span>Select a user please</span>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ lesson14-hooks/task2/src/User.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+// 0. create project +
+// 1. male static layout (верстка) +
+// 2. divide into components +
+// 3. static react version (верстка) +
+// 4. declare state (what, where) and props ++
+//5. write logic +++
+
+const User = ({ match }) => {
+  const { usersId } = match.params;
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${usersId}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        throw new Error();
+      })
+      .then(userData => {
+        setUserData(userData);
+      });
+  }, [usersId]);
+
+  if (!userData) {
+    return null;
+  }
+  console.log(userData);
+
+  const { name, location, avatar_url } = userData;
+
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
+      </div>
+    </div>
+  );
+};
+
+export default User;
