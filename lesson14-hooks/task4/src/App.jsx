94ee9e4e18ab247261ac766fb8ef5bb9a425bcce
vitalekp <vitalekpushkarov@gmail.com>
2022-06-20 14:29:20 +0300

    connection

---
+++ lesson14-hooks/task4/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ConnectionStatus from './ConnectionStatus';
+
+const App = () => {
+  return <ConnectionStatus />;
+};
+
+export default App;
---
+++ lesson14-hooks/task4/src/ConnectionStatus.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
+
+// 0. create project +
+// 1. male static layout (верстка) +
+// 2. divide into components +
+// 3. static react version (верстка) +
+// 4. declare state (what, where) and props ++
+//5. write logic +++++
+
+// props
+// status
+
+// {classNames('list-item', { 'list-item_done': done })}
+
+const ConnectionStatus = () => {
+  const [connection, setStatus] = useState(true);
+
+  const status = connection ? 'online' : 'offline';
+
+  useEffect(() => {
+    const onlineHandler = () => {
+      setStatus({
+        status: true,
+        statusName: 'online',
+      });
+    };
+    const offlineHandler = () => {
+      setStatus({
+        status: false,
+        statusName: 'offline',
+      });
+    };
+
+    window.addEventListener('online', onlineHandler);
+    window.addEventListener('offline', offlineHandler);
+
+    return () => {
+      window.removeEventListener('online', onlineHandler);
+      window.removeEventListener('offline', offlineHandler);
+    };
+  }, []);
+  return <div className={classNames('status', { status_offline: !connection })}>{status}</div>;
+};
+
+export default ConnectionStatus;
